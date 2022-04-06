e110dc68812506a86b3837a786cb68a9f7d47d2e
Renan <renanferncmrc@gmail.com>
2022-04-06 18:17:50 -0300

    stop using WebSockets

--- main.py
+++ main.py
@@ -1,76 +1,33 @@
 """Fetch the last games of Double (blaze.com) by calculating their seeds"""
-
-
 from utils import calc_seed, get_previous_seeds
-
 
 import aiohttp
 import asyncio
-import websockets
 import ujson
 
-
-client_session = None
-
-
-blaze_ws_url = "wss://api-v2.blaze.com/replication/?EIO=3&transport=websocket"
-blaze_api_games_url = "https://blaze.com/api/roulette_games/{}"
-
+blaze_api_games_url = "https://blaze.com/api/roulette_games/recent"
 
 seeds_amount = 1000 # Amount of seeds to calculate
 
-
-async def parse_payload(payload):
-	data = None
-
-	_id = payload["id"]
-
-	async with client_session.get(blaze_api_games_url.format(_id)) as response:
-		if response.status == 200:
-			data = await response.json()
-
-	if data:
-		seed = data.get("server_seed")
-		if seed:
-			with open("./last_games.json", "w+") as f:
-				ujson.dump(list(
-					map(calc_seed, get_previous_seeds(seed, seeds_amount))
-				), f)
-		else:
-			print(f"Seed of roll with id `{_id}` was not found.")
-	else:
-		print(f"Data of roll with id `{_id}` was not found.")
-
-
-async def receive_message(ws):
-	while True:
-		message = await ws.recv()
-		if isinstance(message, str):
-			if "roulette.update" in message:
-				payload = ujson.loads(message[2:])[1]["payload"]
-				if payload["status"] == "complete":
-					await parse_payload(payload)
-
-			await ws.send("2")
-
-
 async def main():
-	global client_session
-	client_session = aiohttp.ClientSession()
+	async with aiohttp.ClientSession() as session:
+		async with session.get(blaze_api_games_url) as response:
+			if response.ok:
+				data = await response.json() # Supposed to be a list
+				if len(data) > 0:
+					seed = data[0].get("server_seed") # Last game of Double is first in list
+					if seed:
+						with open("./last_games.json", "w+") as f:
+							result = list(map(calc_seed, get_previous_seeds(seed, seeds_amount)))
+							ujson.dump(result, f)
 
-	print("Running websockets...")
-
-	async for ws in websockets.connect(blaze_ws_url):
-		try:
-			await ws.send('420["cmd",{"id":"subscribe","payload":{"room":"chat_room_2"}}]')
-			await ws.send('421["cmd",{"id":"subscribe","payload":{"room":"roulette"}}]')
-
-			print("Connection established.")
-
-			await receive_message(ws)
-		except websockets.ConnectionClosed:
-			continue
-
+						print(f"Successfully calculated a total of {len(result)} seeds")
+					else:
+						print("Invalid data: `server_seed` parameter not found")
+				else:
+					print("Failed to parse data because it's empty")
+			else:
+				print("Blaze API sent an error response")
 
 if __name__ == "__main__":
 	asyncio.run(main())
--- utils.py
+++ utils.py
@@ -8,11 +8,10 @@
 	Official information can be found in
 	https://blaze.com/pt/provably-fair/double
 """
-
+from typing import AnyStr, ByteString, List
 
 import hashlib
 import hmac
-
 
 tiles = {
 	0: "white",
@@ -32,21 +31,27 @@ tiles = {
 	4: "red"
 }
 
-
 """hash of bitcoin block 570120 (https://medium.com/@blazedev/blaze-com-double-seeding-event-d3290ef13454)"""
 salt = b"0000000000000000002aeb06364afc13b3c4d52767e8c91db8cdb39d8f71e8dd"
 
+def get_previous_seeds(server_seed: AnyStr, amount: int) -> List[ByteString]:
+	"""Get the seeds prior to `server_seed`"""
+	if isinstance(server_seed, str):
+		server_seed = server_seed.encode()
 
-def get_previous_seeds(server_seed, amount):
-	chain = [server_seed.encode()]
+	chain = [server_seed]
 	for _ in range(amount):
 		_hash = hashlib.sha256()
 		_hash.update(chain[-1])
 		chain.append(_hash.hexdigest().encode())
 	return chain
 
+def calc_seed(seed: AnyStr):
+	"""Calculate the hash of `seed` in order to parse the obtained hash,
+		resulting in the stats (color, roll number, and server_seed) of it
+	"""
+	if isinstance(seed, str):
+		seed = seed.encode()
 
-def calc_seed(seed):
-	_hash = hmac.new(seed, salt, hashlib.sha256).hexdigest()
-	n = int(float.fromhex(_hash) % 15)
-	return { "color": tiles[n], "roll": n, "server_seed": seed.decode(), "hash": _hash }
+	n = int(float.fromhex(hmac.new(seed, salt, hashlib.sha256).hexdigest()) % 15)
+	return { "color": tiles[n], "roll": n, "server_seed": seed.decode() }
